de0ab27867cd033ca6ca5225497612f787ba6cb6
Chung Tran Phuong <tranphuongchungit@gmail.com>
2024-12-28 13:09:12 +0900

    release 1.16

--- src/UserCustomizeData.cpp
+++ src/UserCustomizeData.cpp
@@ -1,3 +1,11 @@
+/*#*******************************************************************************
+# COPYRIGHT NOTES
+# ---------------
+# This is a part of VinaText Project
+# Copyright(C) - free open source - vinadevs
+# This source code can be used, distributed or modified under MIT license
+#*******************************************************************************/
+
 #include "stdafx.h"
 #include "UserCustomizeData.h"
 #include "PathUtil.h"
